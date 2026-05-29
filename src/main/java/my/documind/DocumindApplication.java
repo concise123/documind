c097a49162a9433cd807d8c3841e75bb8e6841a6
concise123 <kang011235@gmail.com>
2026-05-29 20:55:59 +0900

    init: Spring Boot 프로젝트 초기 생성

---
+++ src/main/java/my/documind/DocumindApplication.java
@@ -0,0 +1,13 @@
+package my.documind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumindApplication.class, args);
+	}
+
+}
