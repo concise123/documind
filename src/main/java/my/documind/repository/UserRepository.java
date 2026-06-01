a3cd84dc49b06953088e30dd1c69c76e73520bcb
concise123 <kang011235@gmail.com>
2026-06-01 22:56:09 +0900

    feat: User 엔티티 및 Repository 추가

---
+++ src/main/java/my/documind/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package my.documind.repository;
+
+import my.documind.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ src/test/java/my/documind/repository/UserRepositoryTest.java
@@ -0,0 +1,56 @@
+package my.documind.repository;
+
+import lombok.extern.log4j.Log4j2;
+import my.documind.domain.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+@SpringBootTest
+@Log4j2
+public class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    @DisplayName("INSERT 기능 테스트")
+    public void testInsert() {
+        User user = User.builder()
+                .password("password")
+                .email("test@test.com")
+                .nickname("tester")
+                .build();
+        User result = userRepository.save(user);
+        log.info("ID: " + user.getId());
+    }
+
+    @Test
+    @DisplayName("SELECT 기능 테스트")
+    public void testSelect() {
+        Long id = 1L;
+        Optional<User> result = userRepository.findById(id);
+        User board = result.orElseThrow();
+        log.info(board);
+    }
+
+    @Test
+    @DisplayName("UPDATE 기능 테스트")
+    public void testUpdate() {
+        Long id = 1L;
+        Optional<User> result = userRepository.findById(id);
+        User board = result.orElseThrow();
+        board.changeNickname("tester2");
+        userRepository.save(board);
+        log.info(board);
+    }
+
+    @Test
+    @DisplayName("DELETE 기능 테스트")
+    public void testDelete() {
+        Long id = 1L;
+        userRepository.deleteById(id);
+    }
+}
