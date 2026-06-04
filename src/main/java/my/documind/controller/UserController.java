ce4133ba678566ace7a30912a5cd0a81cc8d9b80
concise123 <kang011235@gmail.com>
2026-06-04 22:21:42 +0900

    fix: 회원가입 입력값 검증 추가

--- src/main/java/my/documind/controller/UserController.java
+++ src/main/java/my/documind/controller/UserController.java
@@ -1,6 +1,7 @@
 package my.documind.controller;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import my.documind.domain.User;
@@ -8,6 +9,8 @@ import my.documind.dto.UserLoginDTO;
 import my.documind.dto.UserSignupDTO;
 import my.documind.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,19 +26,28 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/signup")
-    public void signupGET() {
+    public String signupGET(Model model) {
         log.info("----------signup get----------");
+        model.addAttribute("userSignupDTO", new UserSignupDTO());
+        return "user/signup";
     }
 
     @PostMapping("/signup")
-    public String signupPOST(UserSignupDTO userSignupDTO, RedirectAttributes redirectAttributes) {
+    public String signupPOST(@Valid UserSignupDTO userSignupDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
         log.info("----------signup post----------");
         log.info(userSignupDTO);
+        if (bindingResult.hasErrors()) {
+            return "user/signup";
+        }
         try {
             userService.signup(userSignupDTO);
         } catch (IllegalArgumentException e) {
-            redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
-            return "redirect:/user/signup";
+            bindingResult.rejectValue(
+                    "email",
+                    "duplicate",
+                    e.getMessage()
+            );
+            return "user/signup";
         }
         redirectAttributes.addFlashAttribute("result", "success");
         return "redirect:/user/login";
