63a94175793a2d6636ed03f7ee7d96461df7c885
concise123 <kang011235@gmail.com>
2026-06-20 20:10:03 +0900

    test: contextLoads 테스트에 test 프로필 적용

--- src/test/java/my/documind/DocumindApplicationTests.java
+++ src/test/java/my/documind/DocumindApplicationTests.java
@@ -2,8 +2,10 @@ package my.documind;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DocumindApplicationTests {
 
 	@Test
